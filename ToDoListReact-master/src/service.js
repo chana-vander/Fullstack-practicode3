b95fcd192de1733590dc4c8786fec51578d62721
chana-vander <c89408@gmail.com>
2025-02-06 14:01:09 +0200

    bsd-finished!!!

---
+++ ToDoListReact-master/src/service.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = "http://localhost:5294";
+
+//טיפול בשגיאות:
+axios.interceptors.response.use(
+  //אם אין שגיאה
+  (response)=>{
+    return response
+  },
+  //אם יש שגיאה
+  (error)=>{
+    console.error("error in Api: ",error.response?.status,error.message);
+    return Promise.reject(error);
+  }  
+);
+
+export default {
+  getTasks: async () => {
+    const result = await axios.get(`/tasks`)    
+    return result.data;
+  },
+
+  addTask: async(name)=>{
+    console.log('addTask', name)
+    const result=await axios.post(`/tasks`,{name,isComplete:false});
+    return result.data;
+  },
+
+  setCompleted: async(id, isComplete)=>{
+    console.log('setCompleted', {id, isComplete})
+    const result=await axios.put(`/tasks/${id}`,{isComplete});
+    return result.data;
+  },
+
+  deleteTask:async(id)=>{
+    console.log('deleteTask');
+    const result=await axios.delete(`/tasks/${id}`);
+    return result.data;
+  }
+};
