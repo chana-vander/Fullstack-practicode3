e1c1764d2f6dbbd402fb67e0e6d213a89d886d75
chana-vander <c89408@gmail.com>
2025-02-06 13:55:00 +0200

    bsd-finish!!!

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using TodoApi;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,13 +8,51 @@ builder.Services.AddDbContext<ToDoDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("ToDoDB"),
     new MySqlServerVersion(new Version(8,0,33))));
 
+//cors:
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin()    //מתיר קריאות מכל כתובת
+              .AllowAnyMethod()    //מתיר GET, POST, PUT, DELETE וכו'
+              .AllowAnyHeader());  //מתיר כל כותרות הבקשה
+});
+
+//var app = builder.Build();
+
+//swagger:
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ToDo API",
+        Version = "v1"
+    });
+});
+
 var app = builder.Build();
+
+//הפעלת Swagger במצב Development
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "ToDo API v1");
+    });
+}
+app.UseCors("AllowAll");
 app.UseRouting();
 
 //functions:
 app.MapGet("/tasks", async (ToDoDbContext db) =>{
-  var items=await db.Items.ToListAsync();
-  return Results.Ok(items);
+  var tasks=await db.Items.ToListAsync();
+  return Results.Ok(tasks);
+});
+
+app.MapGet("/tasks/{id}", async (int id, ToDoDbContext db) => {
+    var task = await db.Items.FindAsync(id);
+    return task != null ? Results.Ok(task) : Results.NotFound();
 });
 
 app.MapPost("/tasks", async (ToDoDbContext db, Item newItem) =>
@@ -45,5 +84,7 @@ app.MapDelete("/tasks/{id}",async(int id,ToDoDbContext db)=>{
   await db.SaveChangesAsync();
   return Results.NoContent();
 });
+//defalt:
+// app.MapGet("/", ()=>"Hello");
 
 app.Run();
