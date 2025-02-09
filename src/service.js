bcd1e5a8a7005913d434948e411ae9c36db097ac
chana-vander <c89408@gmail.com>
2025-02-09 15:58:03 +0200

    bsd-update

--- src/service.js
+++ src/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://localhost:5294";
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
 
 //טיפול בשגיאות:
 axios.interceptors.response.use(
